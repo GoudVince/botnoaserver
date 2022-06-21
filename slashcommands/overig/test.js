ca9229a8d896868fca43fe70c468e6ffec02bab1
GoudVince <107935735+GoudVince@users.noreply.github.com>
2022-06-21 17:41:20 +0200

    Add files via upload

--- slashcommands/overig/test.js
+++ slashcommands/overig/test.js
@@ -1,1 +1,19 @@
-//hoi
+const Discord = require('discord.js');
+
+module.exports.run = async (client, inter) => {
+
+    if(!inter.member.permissions.has('KICK_MEMBERSS')){
+        return await inter.reply({ content: 'jij hebt hier geen perms voor'})
+    }
+
+    const user = inter.options.getUser('gebruiker');
+
+    setTimeout(() => {inter.guild.members.kick(user.id).catch(() => {})}, 1000)
+
+    return await inter.reply(`${user} is succesvol gekickt`)
+
+}
+
+module.exports.help = {
+    name: 'kick'
+}
