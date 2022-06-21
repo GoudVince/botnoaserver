9f34b161ae435e90f4cbb94c7d3c6a5cf24a5a75
GoudVince <vvanderdong@yahoo.com>
2022-06-21 18:22:11 +0200

    Update test.js

--- slashcommands/overig/test.js
+++ slashcommands/overig/test.js
@@ -18,24 +18,3 @@ module.exports.run = async (client, inter) => {
 module.exports.help = {
     name: 'kick'
 }
-
-const Discord = require('discord.js');
-
-module.exports.run = async (client, inter) => {
-
-    if(!inter.member.permissions.has('KICK_MEMBERSS')){
-        return await inter.reply({ content: 'jij hebt hier geen perms voor'})
-    }
-
-    const user = inter.options.getUser('gebruiker');
-
-    setTimeout(() => {inter.guild.members.kick(user.id).catch(() => {})}, 1000)
-
-    return await inter.reply(`${user} is succesvol gekickt`)
-
-}
-
-module.exports.help = {
-    name: 'kick',
-ca9229a8d896868fca43fe70c468e6ffec02bab1
-}
