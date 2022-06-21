38dc2dba5389c2b62f48d09f670247e70ac86301
GoudVince <vvanderdong@yahoo.com>
2022-06-21 18:37:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const client = new Client({
  
 client.on("ready", () => {
     console.log((`${client.user.username} is klaar voor gebruik`))
-    client.user.setActivity("iedereen", { type: 'WATCHING' });
+    client.user.setActivity("iedereen👀", { type: 'WATCHING' });
     client.user.setStatus("dnd")
  
 });
