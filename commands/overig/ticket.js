6aa0a24d8d06dbd89b948cc0239925c12f6ba77b
GoudVince <vvanderdong@yahoo.com>
2022-06-22 14:29:30 +0200

    Create ticket.js

---
+++ commands/overig/ticket.js
@@ -0,0 +1,50 @@
+const discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+
+    const categoryID = "988139409377493092";
+
+    var userName = message.author.username;
+
+    var userDiscriminator = message.author.discriminator;
+
+    var reason = args.join(" ");
+    if(!reason) return message.channel.send("Gelieve een reden mee geven");
+    
+    var ticketBestaat = false;
+
+    message.guild.channels.cache.forEach((channel) => {
+
+        if(message.name == userName.toLowerCase() + "-" + userDiscriminator) {
+
+            message.channel.send("Je hebt al een ticket aangemaakt!");
+
+            ticketBestaat = true;
+
+            return;
+
+         }
+
+    });
+
+    if(ticketBestaat) return;
+
+    message.guild.channels.create(userName.toLowerCase() + "-" + userDiscriminator, {type: "text"}).then((createdChan) => {
+
+        createdChan.setParent(categoryID).then((settedParent) => {
+
+
+
+            
+
+        })
+
+    })
+
+}
+
+module.exports.help = {
+    name: 'ticket',
+    description: 'Maak een ticket aan',
+    category: 'info',
+}
