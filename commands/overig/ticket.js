7252334a355d2c3692be8726520dd3750ed82320
GoudVince <vvanderdong@yahoo.com>
2022-06-22 20:07:02 +0200

    Update ticket.js

--- commands/overig/ticket.js
+++ commands/overig/ticket.js
@@ -33,49 +33,6 @@ module.exports.run = async (client, message, args) => {
 
         createdChan.setParent(categoryID).then((settedParent) => {
 
-            // Perms zodat iedereen niets kan lezen.
-            settedParent.permissionOverwrites.edit(message.guild.roles.cache.find(x => x.name === "@everyone"), {
- 
-                SEND_MESSAGES: false,
-                VIEW_CHANNEL: false
- 
-            });
- 
-            // READ_MESSAGE_HISTORY Was vroeger READ_MESSAGES
-            // Perms zodat de gebruiker die het command heeft getypt alles kan zien van zijn ticket.
-            settedParent.permissionOverwrites.edit(message.author.id, {
-                CREATE_INSTANT_INVITE: false,
-                READ_MESSAGE_HISTORY: true,
-                SEND_MESSAGES: true,
-                ATTACH_FILES: true,
-                CONNECT: true,
-                ADD_REACTIONS: true
-            });
-            
-            // Perms zodat de gebruikers die admin zijn alles kunnen zien van zijn ticket.
-            settedParent.permissionOverwrites.edit(message.guild.roles.cache.find(x => x.name === "Moderator"), {
-                CREATE_INSTANT_INVITE: false,
-                READ_MESSAGE_HISTORY: true,
-                SEND_MESSAGES: true,
-                ATTACH_FILES: true,
-                CONNECT: true,
-                ADD_REACTIONS: true
-            });
-            
-            let today = new Date();
-            let dd = String(today.getDate()).padStart(2, '0'); // Nul toevoegen als het bv. 1 is -> 01
-            let mm = String(today.getMonth() + 1).padStart(2, '0');
-            let yyyy = today.getFullYear();
-            today = `${dd}/${mm}/${yyyy}`;
-
-            let embedParent = new discord.MessageEmbed()
-                .setAuthor(message.author.username, message.author.displayAvatarURL({ size: 4096 }))
-                .setTitle('Nieuw ticket')
-                .addFields(
-                    {name: "Reden", value: reason , inline: true},
-                    {name: "Aangemaakt op", value: reason , inline: true}
-                );
-
             message.channel.send('✅ Ticket aangemaakt.'); 
 
             settedParent.send({embeds: [embedParent] });
