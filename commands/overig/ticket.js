77bc1251952155389edd5f7581da92c8b89d8971
GoudVince <vvanderdong@yahoo.com>
2022-06-22 19:03:07 +0200

    Update ticket.js

--- commands/overig/ticket.js
+++ commands/overig/ticket.js
@@ -69,7 +69,7 @@ module.exports.run = async (client, message, args) => {
             today = `${dd}/${mm}/${yyyy}`;
 
             let embedParent = new discord.MessageEmbed()
-                .setAuthor(message.author.username, message.author.displayAvatarURL({ size: 4096 }))
+                .setauthor(message.author.username, message.author.displayAvatarURL({ size: 4096 }))
                 .setTitle('Nieuw ticket')
                 .addFields(
                     {name: "Reden", value: reason , inline: true},
